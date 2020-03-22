4eed013e138150b9a382fe30be74d13251789d32
Yeonggyun.Kim <kygha7205@naver.com>
2020-03-23 03:41:50 +0900

    Add functin that introduces me

--- training 1.py
+++ training 1.py
@@ -2,6 +2,11 @@ def main():
     a = input()
     if int(a) == 1:
         print("Yeonggyun Kim")
+    find2(a)
 
+
+def find2(input):
+    if int(input) == 2:
+        print("Hello. My name is Yeonggyun Kim. I'm 25 years old and I live in seoul.")
 
 main()
